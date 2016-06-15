efe6e7c4f5711d63927677584c02b0a5c639e1c0
yigithanyilmaz <yyilmaz@thoughtworks.com>
2016-06-15 14:32:01 +0300

    different Denominators tests added

--- com.thoughtworks.tdd/src/test/java/FractionEqualsTest.java
+++ com.thoughtworks.tdd/src/test/java/FractionEqualsTest.java
@@ -1,3 +1,4 @@
+import com.sun.prism.impl.FactoryResetException;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -5,6 +6,20 @@ public class FractionEqualsTest {
     @Test
     public void sameNumeratorAndDenominator() throws Exception {
         assertEquals(new Fraction(3,5), new Fraction(3,5));
+
+    }
+
+    @Test
+    public void differentNumberAndDenominator() throws Exception {
+        assertNotEquals(new Fraction(2, 54), new Fraction(31, 35));
+
+
+
+    }
+
+    @Test
+    public void differentDominators() throws Exception {
+        assertNotEquals(new Fraction(3,4), new Fraction(3,555));
 
     }
 }
