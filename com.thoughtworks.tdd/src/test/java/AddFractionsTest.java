2d798f0760c76a35003af1bcf92cfe1d5a4eb829
yigithanyilmaz <yyilmaz@thoughtworks.com>
2016-06-22 15:43:12 +0300

    Added reduce additions

--- com.thoughtworks.tdd/src/test/java/AddFractionsTest.java
+++ com.thoughtworks.tdd/src/test/java/AddFractionsTest.java
@@ -1,3 +1,4 @@
+import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -13,6 +14,7 @@ public class AddFractionsTest {
     @Test
     public void zeroPlusZero() throws Exception {
         checkFractionsAsIntegers(0, 0, 0);
+
     }
 
     @Test
@@ -40,10 +42,8 @@ public class AddFractionsTest {
 
     @Test
     public void nonTrivialDemoniator() throws Exception {
-
         final Fraction sum = new Fraction(1, 5).plus(new Fraction(2, 5));
         assertEquals(new Fraction(3, 5), sum);
-
 
     }
 
@@ -55,13 +55,47 @@ public class AddFractionsTest {
 
     @Test
     public void differentDenominatorsWithoutreduce() throws Exception {
-        assertEquals(new Fraction(5,6), new Fraction(1,2).plus(new Fraction(1,3)));
+        assertEquals(new Fraction(5, 6), new Fraction(1, 2).plus(new Fraction(1, 3)));
 
     }
 
     @Test
     public void reducedResultToWholeNumber() throws Exception {
-        assertEquals(new Fraction(1),new Fraction(1,3).plus(new Fraction(2,3)));
+        assertEquals(new Fraction(1), new Fraction(1, 3).plus(new Fraction(2, 3)));
+
+    }
+
+    @Test
+    public void oneDenominatorIsMultopleOfTheOther() throws Exception {
+        assertEquals(new Fraction(11, 8), new Fraction(3, 4).plus(new Fraction(5, 8)));
+
+    }
+
+    @Test
+    public void commonFactorInDenominators() throws Exception {
+        assertEquals(new Fraction(11, 18), new Fraction(1, 6).plus(new Fraction(4, 9)));
+
+    }
+
+    @Test
+    public void reduceResultEvenDenominatorsAreTheSame() throws Exception {
+        assertEquals(new Fraction(3, 2), new Fraction(3, 4).plus(new Fraction(3, 4)));
+
+    }
+
+    @Test
+    public void negativeFractionAndReducing() throws Exception {
+        assertEquals(new Fraction(1, 2), new Fraction(-1, 4).plus(new Fraction(3, 4)));
+        assertEquals(new Fraction(-1, 8), new Fraction(3, 8).plus(new Fraction(-1, 2)));
+
+
+    }
+
+    @Test
+    @Ignore("constructor doesnt avoid negative numbers")
+    public void negativeSignsEverywhere() throws Exception {
+        assertEquals(new Fraction(1, 2), new Fraction(1,-4).plus(new Fraction(-3, -4)));
+
 
     }
 }
