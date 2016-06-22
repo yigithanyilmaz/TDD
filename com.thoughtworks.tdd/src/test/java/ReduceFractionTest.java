3efb14cb9d9c404e251ae335f61d89b6e3acb1b6
yigithanyilmaz <yyilmaz@thoughtworks.com>
2016-06-22 13:09:47 +0300

    Added reducing tests

--- com.thoughtworks.tdd/src/test/java/ReduceFractionTest.java
+++ com.thoughtworks.tdd/src/test/java/ReduceFractionTest.java
@@ -13,5 +13,6 @@ public class ReduceFractionTest {
     @Test
     public void reduceToNotWholeNumber() throws Exception {
         assertEquals(new Fraction(3,4), new Fraction(6,8));
+        assertEquals(new Fraction(6),new Fraction(24,4));
     }
 }
